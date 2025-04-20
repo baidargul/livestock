efee696a92b80fb983f8a5afca2a638819d6edf7
Baidar Gul <baidargul@outlook.com>
2025-04-21 00:44:43 +0500

    ...

--- app/(root)/(auth)/signin/page.tsx
+++ app/(root)/(auth)/signin/page.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const page = (props: Props) => {
     return (
-        <div className='relative w-full min-h-screen' style={{ backgroundImage: `url(./media/2025-04-20_21-45-18_7037.png)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundPositionX: "-140px", backgroundPositionY: "-20px" }}>
+        <div className='relative w-full min-h-screen' style={{ backgroundImage: `url(./media/2025-04-20_21-45-18_7037.png)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundPositionX: "-200px", backgroundPositionY: "-20px" }}>
             <div className='rounded-t-[90px] transition-all duration-200 ease-in-out bg-white absolute bottom-0 z-20 w-full h-1/2 p-10 pt-5 sm:pt-10' style={{ boxShadow: "1px 11px 20px 9px #f60405" }}>
                 <Client />
             </div>
