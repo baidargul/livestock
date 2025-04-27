ea52f5ba5d4a8bfedf6db33340bb69f6a46713da
Baidar Gul <baidargul@outlook.com>
2025-04-27 16:49:47 +0500

    changed styles for type also

--- app/(root)/(session)/animal/add/_components/SelectAnimal.tsx
+++ app/(root)/(session)/animal/add/_components/SelectAnimal.tsx
@@ -88,13 +88,40 @@ const SelectAnimal = (props: Props) => {
                 {
                     animals.map((animal: any) => {
                         return (
-                            <div key={animal.id} className={`relative w-32 h-32 transition-all duration-200 ease-in-out flex flex-col items-center justify-center border-2 border-gray-300 rounded-lg p-4 cursor-pointer  ${props.animal?.type && props.animal?.type === animal.name.toLocaleLowerCase() ? "bg-emerald-700 text-emerald-700" : "hover:bg-gray-100"}`} onClick={() => handleSelectAnimal(animal)}>
-                                <Image src={animal.image} alt={animal.name} priority layout='fixed' width={100} height={100} className={`w-full h-full absolute inset-0 z-0 object-cover mb-2 rounded-lg ${props.animal?.type && props.animal?.type === animal.name.toLocaleLowerCase() ? "p-2 rounded-xl" : ``} ${props.animal?.type && props.animal?.type !== animal.name.toLocaleLowerCase() ? "grayscale blur-[1px] opacity-70" : ``}`} />
-                                <h1 className={`text-lg absolute z-10 bottom-0 left-1/2 -translate-x-1/2 font-semibold transition-all duration-200 ease-in-out ${props.animal?.type && props.animal?.type === animal.name.toLocaleLowerCase() ? "bg-emerald-700" : "bg-black"}  w-full text-white text-center`}>{animal.name}</h1>
+                            <div
+                                key={animal.id}
+                                className={`relative w-56 sm:w-[400px] h-32 transition-all duration-200 ease-in-out flex flex-col items-center justify-center border-2 border-gray-300 rounded-lg p-4 cursor-pointer ${props.animal?.type && props.animal?.type === animal.name.toLocaleLowerCase() ? "" : "hover:bg-gray-100"
+                                    }`}
+                                onClick={() => handleSelectAnimal(animal)}
+                            >
+                                <Image
+                                    src={animal.image}
+                                    alt={animal.name}
+                                    priority
+                                    layout="fixed"
+                                    width={100}
+                                    height={100}
+                                    className={`w-full h-full absolute inset-0 z-0 object-cover mb-2 rounded-lg ${props.animal?.type && props.animal?.type === animal.name.toLocaleLowerCase() ? "" : ""
+                                        } ${props.animal?.type && props.animal?.type !== animal.name.toLocaleLowerCase() ? "grayscale blur-[1px] opacity-70" : ""
+                                        }`}
+                                />
+                                <div
+                                    className={`text-lg absolute z-10 bottom-0 left-1/2 -translate-x-1/2 font-semibold transition-all duration-200 ease-in-out ${props.animal?.type && props.animal?.type === animal.name.toLocaleLowerCase()
+                                        ? "bg-gradient-to-t from-emerald-700/40 to-transparent h-10"
+                                        : "bg-gradient-to-t from-black/50 to-transparent h-14"
+                                        } w-full text-white text-center`}
+                                ></div>
+                                <h1
+                                    className={`text-lg absolute z-10 bottom-2 left-2 font-semibold transition-all duration-200 ease-in-out w-full text-white`}
+                                    style={{ textShadow: "1px 1px 2px black" }}
+                                >
+                                    {animal.name}
+                                </h1>
                             </div>
-                        )
+                        );
                     })
                 }
+
             </div>
             <div className='flex items-center justify-between gap-4 w-full p-4'>
                 <Button onClick={() => { window.history.back() }} className='w-full' variant='btn-secondary'>Back</Button>
