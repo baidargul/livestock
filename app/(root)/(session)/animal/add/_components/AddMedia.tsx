244d689be57c9e01acdc78cbd1202bf1cd5f7e5b
Baidar Gul <baidargul@outlook.com>
2025-05-03 12:11:00 +0500

    working with images ui

--- app/(root)/(session)/animal/add/_components/AddMedia.tsx
+++ app/(root)/(session)/animal/add/_components/AddMedia.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/ui/Button'
 import { FileImageIcon } from 'lucide-react'
 import React from 'react'
 
@@ -10,10 +11,15 @@ type Props = {
 
 const AddMedia = (props: Props) => {
     return (
-        <div className='select-none'>
+        <div className='w-full min-h-[100dvh] flex flex-col items-center gap-4 justify-between p-4 select-none'>
+            <div className='text-xl font-semibold tracking-tight text-center'>Please select atleast 4 images</div>
             <div className='p-2 bg-emerald-100 cursor-pointer border-emerald-400 flex flex-col justify-center items-center rounded-xl border' style={{ boxShadow: "0px 20px 14px -8px #98d3b5" }}>
                 <FileImageIcon className='w-28 h-28 text-emerald-800' />
-                <div className='text-xl font-bold font-sans text-emerald-800'>Add Media</div>
+                <div className='text-xl font-bold font-sans text-emerald-800'>Select Images</div>
+            </div>
+            <div className='flex items-center justify-between gap-4 w-full p-4'>
+                <Button onClick={props.moveBack} className='w-full' variant='btn-secondary'>Back</Button>
+                <Button onClick={props.moveNext} className='w-full'>Next</Button>
             </div>
         </div>
     )
