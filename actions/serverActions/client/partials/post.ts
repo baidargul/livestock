a1dc6b375b742d01e9cfd9d3b8c44b60e2d25d9b
Baidar Gul <baidargul@outlook.com>
2025-05-07 18:46:04 +0500

    delete api will now purge vercel

--- actions/serverActions/client/partials/post.ts
+++ actions/serverActions/client/partials/post.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 async function removePost(id: string) {
-  const response = await axios.delete(`/api/post?id=${id}`);
+  const response = await axios.delete(`/api/post?id=${id}`, {
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+    },
+  });
   return response;
 }
 
