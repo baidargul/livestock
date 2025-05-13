202a3d533c6033abd48fdb5233d0cc00387b0673
Baidar Gul <baidargul@outlook.com>
2025-05-13 11:05:13 +0500

    changed project name

--- components/controls/MediaViewer.tsx
+++ components/controls/MediaViewer.tsx
@@ -23,7 +23,7 @@ const MediaViewer = (props: Props) => {
             {isOpen && <div onClick={handleClose} className='fixed top-0 left-0 w-full h-full bg-black/70 backdrop-blur-[1px] z-20'></div>}
             <div className={`fixed select-none top-0 left-0 z-30 w-[90%]  h-[90%]flex justify-center items-center transition-all duration-100 ease-in-out delay-150 ${isOpen ? "opacity-100 pointer-events-auto translate-y-0" : "0 opacity-0 translate-y-full pointer-events-none"}`} style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
                 <div className={`bg-white p-4 rounded drop-shadow-2xl w-full h-full`}>
-                    <Image src={props.image} loading='lazy' layout='responsive' width={100} height={100} quality={50} alt='livestock' className=' w-full h-full object-contain' />
+                    <Image src={props.image} loading='lazy' layout='responsive' width={100} height={100} quality={50} alt='janwarmarkaz' className=' w-full h-full object-contain' />
                 </div>
             </div>
             <span
--- components/website/sections/landingpage/LandingPageImage.tsx
+++ components/website/sections/landingpage/LandingPageImage.tsx
@@ -15,7 +15,7 @@ const SectionLandingPageImage = (props: Props) => {
                 <h2 className='tracking-tight font-medium' style={{ textShadow: "1px 1px 3px black" }}>{props.subheading ?? "get your adorable pets home today!"}</h2>
             </div>
             <div className='absolute z-10 top-0 left-0 w-full h-[150%] rounded-t-xl bg-gradient-to-t from-transparent to-emerald-600/50'></div>
-            <Image src={images.chickens.covers[1]} priority layout='fixed' alt='livestock' draggable={false} width={406} height={195} quality={70} className='w-full max-h-[230px] h-full rounded-xl object-cover' />
+            <Image src={images.chickens.covers[1]} priority layout='fixed' alt='janwarmarkaz' draggable={false} width={406} height={195} quality={70} className='w-full max-h-[230px] h-full rounded-xl object-cover' />
         </div>
     )
 }
