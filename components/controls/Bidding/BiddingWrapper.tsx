ee84737907bc9707303045126752f8381d7f2e50
Baidar Gul <baidargul@outlook.com>
2025-05-19 18:35:10 +0500

    fixed bidding wrapper ui

--- components/controls/Bidding/BiddingWrapper.tsx
+++ components/controls/Bidding/BiddingWrapper.tsx
@@ -54,8 +54,8 @@ const BiddingWrapper = (props: Props) => {
 
     return (
         <>
-            <div className={`fixed bottom-0 flex flex-col justify-between gap-0 ${isOpen === true ? "translate-y-0 pointer-events-auto opacity-100" : "translate-y-full pointer-events-none opacity-0"} transition-all duration-300 drop-shadow-2xl border border-emerald-900/30 w-[96%] mx-2 h-[90%] left-0 rounded-t-xl bg-white z-20 p-4`}>
-                <div className='flex flex-col gap-2'>
+            <div className={`fixed bottom-0  flex flex-col justify-between gap-0 ${isOpen === true ? "translate-y-0 pointer-events-auto opacity-100" : "translate-y-full pointer-events-none opacity-0"} transition-all duration-300 drop-shadow-2xl border border-emerald-900/30 w-[96%] mx-2 h-[90%] left-0 rounded-t-xl bg-white z-20 p-4`}>
+                <div className='flex flex-col gap-2 overflow-y-auto h-[80%]'>
                     <div>
                         <div className='text-xl font-semibold'>
                             {props.animal.title}
@@ -68,7 +68,7 @@ const BiddingWrapper = (props: Props) => {
                         {
                             props.animal.images && props.animal.images.length > 0 && props.animal.images.map((image: any, index: number) => {
                                 return (
-                                    <Image src={image.image} width={100} height={100} layout='fixed' priority key={index} className='rounded-md object-contain border border-emerald-800/10 drop-shadow-[2px]' alt={`${props.animal.title}, ${props.animal.type} - ${props.animal.breed}`} />
+                                    <Image src={image.image} width={100} height={100} layout='fixed' priority key={index} className=' rounded-md object-contain border border-emerald-800/10 drop-shadow-[2px]' alt={`${props.animal.title}, ${props.animal.type} - ${props.animal.breed}`} />
                                 )
                             })
                         }
@@ -84,13 +84,15 @@ const BiddingWrapper = (props: Props) => {
                         </div>} */}
                         <CalculatedDescription animal={props.animal} />
                     </div>
+                </div>
+                <div className='w-full fixed bottom-2 left-0 bg-white p-1 px-4 gap-2'>
                     <div className='my-4'>
                         <Textbox label='Offer Price' type='number' onChange={handleOfferChange} value={offerValue} className='text-center tracking-widest' />
                     </div>
-                </div>
-                <div className='w-full fixed bottom-2 left-0 p-1 px-4 gap-2 flex items-center'>
-                    <Button onClick={() => handleOpen(false)} className='w-full' variant='btn-secondary'>Cancel</Button>
-                    <Button onClick={handlePostOffer} className='w-full'>Place Offer</Button>
+                    <div className=' flex items-center gap-2'>
+                        <Button onClick={() => handleOpen(false)} className='w-full' variant='btn-secondary'>Cancel</Button>
+                        <Button onClick={handlePostOffer} className='w-full'>Place Offer</Button>
+                    </div>
                 </div>
             </div>
             <div onClick={() => handleOpen(true)} className='w-full'>
