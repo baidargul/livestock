750c120745d5f1d33585b933c13e2db186ac1d05
Baidar Gul <baidargul@outlook.com>
2025-05-20 11:56:29 +0500

    added sidebar menu

--- components/website/profile/Icon.tsx
+++ components/website/profile/Icon.tsx
@@ -2,15 +2,16 @@ import { images } from '@/consts/images'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import ProfileMenuWrapper from './_components/ProfileMenuWrapper'
 
 type Props = {}
 
 const UserProfileIcon = (props: Props) => {
     return (
         <div>
-            <Link href={'/animal/add'}>
+            <ProfileMenuWrapper>
                 <Image src={images.chickens.covers[3]} width={40} height={40} layout='fixed' loading='lazy' quality={100} alt='janwarmarkaz' className='rounded-full object-cover w-10 h-10' />
-            </Link>
+            </ProfileMenuWrapper>
         </div>
     )
 }
---
+++ components/website/profile/_components/ProfileMenuWrapper.tsx
@@ -0,0 +1,113 @@
+'use client'
+import { actions } from '@/actions/serverActions/actions'
+import { images } from '@/consts/images'
+import { useLoader } from '@/hooks/useLoader'
+import { useSession } from '@/hooks/useSession'
+import { CrossIcon, List, PlusCircleIcon, X } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const ProfileMenuWrapper = (props: Props) => {
+    const [isToggled, setIsToggled] = useState(false)
+
+    const handleToggleMenu = (val: boolean) => {
+        setIsToggled(val)
+    }
+
+    return (
+        <div className=''>
+            <div onClick={() => handleToggleMenu(true)} className='cursor-pointer'>
+                {props.children}
+            </div>
+            <MenuWrapper handleToggleMenu={handleToggleMenu} isToggled={isToggled} />
+        </div>
+    )
+}
+
+export default ProfileMenuWrapper
+
+const MenuWrapper = ({ handleToggleMenu, isToggled }: any) => {
+    const [user, setUser] = useState<any>(null);
+    const getUser = useSession((state: any) => state.getUser);
+    const logoutUser = useSession((state: any) => state.logoutUser);
+    const setLoading = useLoader((state: any) => state.setLoading)
+    const router = useRouter();
+
+    useEffect(() => {
+        const rawUser = getUser();
+        setUser(rawUser);
+    }, [])
+
+    const handleLoggout = async () => {
+        setLoading(true)
+        const response = await actions.client.user.signout({ token: user?.token });
+        if (response.status === 200) {
+            logoutUser();
+            router.push("/");
+        } else {
+            console.log(response);
+        }
+        setLoading(false)
+    }
+
+    return (
+        <div className={`fixed top-0 right-0 w-full h-full bg-white z-20 p-2 transition-all duration-200 ease-in-out ${isToggled ? "translate-x-0" : "translate-x-full"}`}>
+            <div>
+                <div className='font-bold px-2 tracking-wider text-2xl flex justify-between items-center'>
+                    <div>Menu</div>
+                    <div>
+                        <X onClick={() => handleToggleMenu(false)} className='cursor-pointer' />
+                    </div>
+                </div>
+                <div className='flex flex-col'>
+                    <div className='mt-5 p-4 flex gap-2 justify-between bg-white rounded-xl drop-shadow-sm items-center'>
+                        <div className='flex gap-4 items-center'>
+                            <Image src={images.chickens.covers[3]} width={40} height={40} layout='fixed' loading='lazy' quality={100} alt='janwarmarkaz' className='rounded-full object-cover w-10 h-10' onClick={() => handleToggleMenu(false)} />
+                            <div className=''>
+                                <div className='font-semibold text-xl tracking-wide'>{user?.name}</div>
+                                <div className='text-sm tracking-wide -mt-1 italic text-black/60'>{user?.email}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='grid grid-cols-2 gap-2'>
+                        <div className='cursor-pointer mt-5 p-4 flex gap-2 justify-between bg-white rounded-xl drop-shadow-sm items-center'>
+                            <Link href={'/animal/add'}>
+                                <div className='flex flex-col gap-2 items-start'>
+                                    <div>
+                                        <PlusCircleIcon />
+                                    </div>
+                                    <div className=''>
+                                        <div className='font-semibold tracking-wide'>Add new animal</div>
+                                        <div className='text-xs tracking-wide italic text-black/60'>Create a new animal post for sale.</div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className='cursor-pointer mt-5 p-4 flex gap-2 justify-between bg-white rounded-xl drop-shadow-sm items-center'>
+                            <Link href={'/home'}>
+                                <div className='flex flex-col gap-2 items-start'>
+                                    <div>
+                                        <List />
+                                    </div>
+                                    <div className=''>
+                                        <div className='font-semibold tracking-wide'>See all animals</div>
+                                        <div className='text-xs tracking-wide italic text-black/60'>List of all your animals.</div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                    <div onClick={handleLoggout} className='bg-zinc-100 border border-zinc-200/40 cursor-pointer p-4 rounded-xl mt-5 text-center font-semibold tracking-widest'>
+                        Logout
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
