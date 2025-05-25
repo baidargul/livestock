222b9ad36b8f6208ab8ebeee6031cb97fc7768e9
Baidar Gul <baidargul@outlook.com>
2025-05-25 17:16:33 +0500

    ...

--- components/website/profile/Icon.tsx
+++ components/website/profile/Icon.tsx
@@ -31,7 +31,7 @@ const UserProfileIcon = (props: Props) => {
     return (
         <div>
             <ProfileMenuWrapper>
-                <Image src={currentUser && currentUser.profileImage && currentUser.profileImage.length > 0 ? currentUser.profileImage[0].image : images.site.placeholders.userProfile} width={40} height={40} layout='fixed' loading='lazy' quality={100} alt='janwarmarkaz' className='rounded-full object-cover w-10 h-10' />
+                <Image src={currentUser && currentUser.profileImage && currentUser.profileImage.length > 0 ? currentUser.profileImage[0].image : images.site.placeholders.userProfile} draggable={false} width={40} height={40} layout='fixed' loading='lazy' quality={100} alt='janwarmarkaz' className='rounded-full object-cover w-10 h-10 pointer-events-none' />
             </ProfileMenuWrapper>
         </div>
     )
