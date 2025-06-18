16374e2b45a07af3ce47c8fccde54d6f0db3515e
Baidar Gul <baidargul@outlook.com>
2025-06-18 17:15:24 +0500

    better bidwrapper ui

--- components/controls/Bidding/BiddingWrapper.tsx
+++ components/controls/Bidding/BiddingWrapper.tsx
@@ -289,7 +289,7 @@ const BiddingWrapper = (props: Props) => {
 
     return (
         <>
-            <div className={`fixed bottom-14 select-none flex flex-col justify-between gap-0 ${isOpen === true ? "translate-y-0 pointer-events-auto opacity-100" : "translate-y-full pointer-events-none opacity-0"} transition-all duration-300 drop-shadow-2xl border border-emerald-900/30 w-[96%] mx-2 h-[80%] left-0 rounded-t-xl bg-white z-20 p-4`}>
+            <div className={`fixed ${props.staticStyle ? 'bottom-0 h-[95%]' : 'bottom-14 h-[80%]'}  select-none flex flex-col justify-between gap-0 ${isOpen === true ? "translate-y-0 pointer-events-auto opacity-100" : "translate-y-full pointer-events-none opacity-0"} transition-all duration-300 drop-shadow-2xl border border-emerald-900/30 w-[96%] mx-2 left-0 rounded-t-xl bg-white z-20 p-4`}>
                 {!activeBidRoom && [...rooms.myRooms, ...rooms.otherRooms].length === 0 && <div className='flex flex-col gap-2 overflow-y-auto h-[80%]'>
                     <div>
                         <div className='text-xl font-semibold'>
