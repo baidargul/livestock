8abd39ce01af3409352e1747b3d5a7d819697196
Baidar Gul <baidargul@outlook.com>
2025-07-02 20:38:53 +0500

    ...

--- components/website/footer/_components/_roomsWrapper/Room.tsx
+++ components/website/footer/_components/_roomsWrapper/Room.tsx
@@ -1,6 +1,6 @@
 import BiddingWrapper from '@/components/controls/Bidding/BiddingWrapper'
 import { images } from '@/consts/images'
-import { formalizeText } from '@/lib/utils'
+import { formalizeText, formatCurrency, TrimString } from '@/lib/utils'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
@@ -31,7 +31,7 @@ const Room = (props: Props) => {
 
     return (
         animal && <BiddingWrapper animal={animal} room={props.room} staticStyle allowJoinRoomImmediately>
-            <div className='grid grid-cols-2 gap-2 w-full cursor-pointer hover:bg-emerald-50 py-2 bg-white p-2 rounded-md drop-shadow-sm border border-zinc-200'>
+            <div className='grid grid-cols-2 gap-2 w-full cursor-pointer py-2 bg-white p-2 rounded-md drop-shadow-sm border border-zinc-200'>
                 <div>
                     {/* <Image src={animal.images.length > 0 ? animal.images[0].image : images.chickens.images[1]} loading='lazy' layout='fixed' alt='Product List Row' width={1000} height={1000} draggable={false} className='w-full h-[100px] group-hover:scale-105 transition-all duration-300 ease-in-out bg-black select-none object-cover' /> */}
                     <Image src={images.chickens.images[1]} loading='lazy' layout='fixed' alt='Product List Row' width={1000} height={1000} draggable={false} className='w-full h-[100px] group-hover:scale-105 transition-all duration-300 ease-in-out bg-black select-none object-cover' />
@@ -42,7 +42,11 @@ const Room = (props: Props) => {
                     <div className='px-1'>
                         <div className='text-sm italic line-clamp-1'>{formalizeText(animal.title)}</div>
                     </div>
-                    <div className='text-sm px-1 tracking-tight text-emerald-600 bg-emerald-50 p-1 rounded w-fit h-fit'>{unreadBids} new bid{unreadBids > 1 ? 's' : ''}</div>
+                    <div className=''>
+                        <ThatOneBid bids={props.room.bids} user={props.user} index={1} className={`font-semibold tracking-wide ${props.room.bids[props.room.bids.length - 1]?.isSeen && props.room.bids[props.room.bids.length - 1]?.name !== props.user.name ? "bg-emerald-50" : "bg-amber-50"}`} />
+                        <ThatOneBid bids={props.room.bids} user={props.user} index={2} className='opacity-50 bg-zinc-100' />
+                        <ThatOneBid bids={props.room.bids} user={props.user} index={3} className='opacity-10 bg-zinc-100' />
+                    </div>
                 </div>
             </div>
         </BiddingWrapper>
@@ -50,3 +54,19 @@ const Room = (props: Props) => {
 }
 
 export default Room
+
+
+const ThatOneBid = ({ bids, user, index, className }: { bids: any[], user: any, index: number, className?: string }) => {
+    if (bids.length === 0) return null
+
+    const isValidDemand: any = (Number(bids.length) - Number(index) > -1)
+    if (!isValidDemand) return null
+
+
+    return (
+        <div className={`flex justify-between items-center tracking-tight border-b border-zinc-300 p-1 ${className}`}>
+            <div className='text-sm px-1'>{TrimString(bids[bids.length - index]?.user.name === user.name ? 'You' : bids[bids.length - index]?.user.name, 8)}</div>
+            <div className='text-sm px-1'>{formatCurrency(bids[bids.length - index]?.price)}</div>
+        </div>
+    )
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -276,3 +276,10 @@ export function formatDate(
     ...options,
   });
 }
+
+export function TrimString(str: string, maxLength: number) {
+  if (str.length > maxLength) {
+    return str.substring(0, maxLength - 3) + "...";
+  }
+  return str;
+}
