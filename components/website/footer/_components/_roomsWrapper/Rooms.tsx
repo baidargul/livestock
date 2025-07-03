5aa069bdb6a2042c1344af5b66624f4333ef10a8
Baidar Gul <baidargul@outlook.com>
2025-07-03 17:10:59 +0500

    ...

--- components/website/footer/_components/_roomsWrapper/Rooms.tsx
+++ components/website/footer/_components/_roomsWrapper/Rooms.tsx
@@ -70,7 +70,7 @@ const Rooms = (props: Props) => {
     return (
         isMounted && <div className='w-full h-full text-zinc-700'>
             <div onClick={() => handleSelectSection("myRooms")} className='flex p-2 justify-between items-center bg-zinc-100'>
-                <div className='text-zinc-700 font-semibold text-lg tracking-tight'>Selling</div>
+                <div className='text-zinc-700 font-semibold text-lg tracking-tight'>Selling Rooms</div>
                 <ChevronLeftIcon size={20} className={`transition-all duration-300 ease-in-out ${currentSection === "myRooms" ? "-rotate-90" : ""}`} />
             </div>
             <section style={{ height: currentSection === "myRooms" ? "90%" : currentSection === "otherRooms" ? "0px" : "50%" }} className='p-2 cursor-pointer border-b border-zinc-400 mb-2 bg-gradient-to-b from-zinc-100 to-transparent transition-all duration-300 ease-in-out flex flex-col gap-1 w-full'>
@@ -112,7 +112,7 @@ const Rooms = (props: Props) => {
                 }
             </section>
             <div onClick={() => handleSelectSection("otherRooms")} className='flex p-2 justify-between items-center bg-zinc-100'>
-                <div className='text-zinc-700 font-semibold text-lg tracking-tight'>Buying</div>
+                <div className='text-zinc-700 font-semibold text-lg tracking-tight'>Buying Rooms</div>
                 <ChevronLeftIcon size={20} className={`transition-all duration-300 ease-in-out ${currentSection === "otherRooms" ? "-rotate-90" : ""}`} />
             </div>
             <section style={{ height: currentSection === "otherRooms" ? "85%" : currentSection === "myRooms" ? "0px" : "50%" }} className='p-2 border-b border-zinc-400 bg-gradient-to-b from-zinc-100 to-transparent cursor-pointer transition-all duration-300 ease-in-out flex flex-col gap-1 w-full'>
