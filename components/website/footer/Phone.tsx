3bdef64cfb97f772fd9a686a33233d3f8b3d754f
Baidar Gul <baidargul@outlook.com>
2025-07-05 15:50:33 +0500

    navigation

--- components/website/footer/Phone.tsx
+++ components/website/footer/Phone.tsx
@@ -1,6 +1,7 @@
+'use client'
 import { CompassIcon, HouseIcon, ShoppingBagIcon, UserIcon } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import ProfileMenuWrapper from '../profile/_components/ProfileMenuWrapper'
 import RoomsWrapper from './_components/RoomsWrapper'
 import FetchLastestRooms from '@/components/Fetchers/FetchLastestRooms'
@@ -8,20 +9,36 @@ import FetchLastestRooms from '@/components/Fetchers/FetchLastestRooms'
 type Props = {}
 
 const PhoneFooter = (props: Props) => {
+    const [route, setRoute] = useState('')
+
+    useEffect(() => {
+        setRoute(window.location.pathname)
+    }, [])
+
+
+
     return (
         <div className='bg-emerald-100 text-emerald-600 z-50 sticky bottom-0 left-0 w-full h-14 flex justify-evenly items-center'>
             <FetchLastestRooms />
             <Link href={'/home'}>
-                <HouseIcon />
+                <div className={route.includes("home") ? "bg-emerald-50 border-b-2 border-emerald-200 rounded p-1 px-2" : ""}>
+                    <HouseIcon />
+                </div>
             </Link>
             <Link href={'/nearby'}>
-                <CompassIcon />
+                <div className={route.includes("nearby") ? "bg-emerald-50 border-b-2 border-emerald-200 rounded p-1 px-2" : ""}>
+                    <CompassIcon />
+                </div>
             </Link>
             <RoomsWrapper>
-                <ShoppingBagIcon />
+                <div className={route.includes("cart") ? "bg-emerald-50 border-b-2 border-emerald-200 rounded p-1 px-2" : ""}>
+                    <ShoppingBagIcon />
+                </div>
             </RoomsWrapper>
             <ProfileMenuWrapper>
-                <UserIcon />
+                <div className={route.includes("profile") ? "bg-emerald-50 border-b-2 border-emerald-200 rounded p-1 px-2" : ""}>
+                    <UserIcon />
+                </div>
             </ProfileMenuWrapper>
         </div>
     )
