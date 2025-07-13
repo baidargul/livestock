e51c3cbd6395fb889f6de8c86a6012dd5a474f0e
Baidar Gul <baidargul@outlook.com>
2025-07-13 18:36:46 +0500

    fixed media gallery bug

--- components/controls/MediaViewer.tsx
+++ components/controls/MediaViewer.tsx
@@ -23,10 +23,10 @@ const MediaViewer = (props: Props) => {
     return (
         <>
             {isOpen && <div onClick={handleClose} className='fixed top-0 left-0 w-full h-full bg-black/70 backdrop-blur-[1px] z-20'></div>}
-            <div className={`fixed select-none top-0 left-0 z-30 w-[60%] h-[60%] flex justify-center items-center transition-all duration-100 ease-in-out delay-150 ${isOpen ? "opacity-100 pointer-events-auto translate-y-0" : "0 opacity-0 translate-y-full pointer-events-none"}`} style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
-                <div className={`bg-white p-4 rounded drop-shadow-2xl w-full h-full relative`}>
-                    <XIcon className='absolute top-2 right-2 bg-blend-difference cursor-pointer' onClick={handleClose} />
-                    <Image src={props.image ?? images.chickens.images[1]} loading='lazy' layout='responsive' width={100} height={100} quality={50} alt='janwarmarkaz' className=' w-full h-full object-contain' />
+            <div className={`fixed select-none top-0 left-0 z-30 w-[400px] h-[400px] flex justify-center items-center transition-all duration-100 ease-in-out delay-150 ${isOpen ? "opacity-100 pointer-events-auto translate-y-0" : "0 opacity-0 translate-y-full pointer-events-none"}`} style={{ top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+                <div className={`bg-white p-4 rounded drop-shadow-2xl w-[400px] h-[400px] relative`}>
+                    <XIcon className='absolute top-5 right-5 bg-blend-difference cursor-pointer' onClick={handleClose} />
+                    <Image src={props.image ?? images.chickens.images[1]} loading='lazy' layout='fixed' width={100} height={100} quality={50} alt='janwarmarkaz' className=' w-full h-full object-cover' />
                 </div>
             </div>
             <span
--- components/ui/ProductGallery.tsx
+++ components/ui/ProductGallery.tsx
@@ -21,10 +21,10 @@ const ProductGallery = (props: Props) => {
                 </div>
                 <div className='flex flex-col gap-1 overflow-y-auto max-h-[400px]'>
                     {
-                        [...otherImages, ...otherImages].map((image: any) => {
+                        [...otherImages, ...otherImages].map((image: any, index: number) => {
 
                             return (
-                                <MediaViewer key={image.id} image={image.image ?? images.chickens.images[1]}>
+                                <MediaViewer key={`${image.id}-${index}`} image={image.image ?? images.chickens.images[1]}>
                                     <Image src={image.image ?? images.chickens.images[1]} width={500} height={500} layout='fixed' loading='lazy' quality={50} alt='janwarmarkaz' className='w-[198px] h-[198px] object-cover' />
                                 </MediaViewer>
                             )
