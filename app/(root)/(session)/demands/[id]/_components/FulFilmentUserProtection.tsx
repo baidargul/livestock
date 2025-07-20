d1a7f9bf13f6a33a805aeb44904e72bb8ac64d95
Baidar Gul <baidargul@outlook.com>
2025-07-20 11:58:14 +0500

    better ui

--- app/(root)/(session)/demands/[id]/_components/FulFilmentUserProtection.tsx
+++ app/(root)/(session)/demands/[id]/_components/FulFilmentUserProtection.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { actions } from '@/actions/serverActions/actions'
 import { useSession } from '@/hooks/useSession'
+import { useUser } from '@/socket-client/SocketWrapper'
 import React, { useEffect, useState } from 'react'
 
 type Props = {
@@ -10,7 +11,7 @@ type Props = {
 
 const FulFilmentUserProtection = (props: Props) => {
     const [isMounted, setIsMounted] = useState(false)
-    const [user, setUser] = useState<any>(null)
+    const user = useUser()
     const [validatingPreviousState, setValidatingPreviousState] = useState(false)
     const [hasPlacedOffer, setHasPlacedOffer] = useState(false)
     const getUser = useSession((state: any) => state.getUser)
@@ -21,9 +22,7 @@ const FulFilmentUserProtection = (props: Props) => {
 
     useEffect(() => {
         if (isMounted) {
-            const rawuser = getUser()
-
-            if (rawuser && rawuser.id !== props.demand.userId) {
+            if (user && user.id !== props.demand.userId) {
                 setValidatingPreviousState(true)
                 const hasPlacedOffer = async () => {
                     const response = await actions.client.demand.hasUserPostedOffer(props.demand.userId, props.demand.id)
@@ -37,9 +36,8 @@ const FulFilmentUserProtection = (props: Props) => {
 
                 hasPlacedOffer()
             }
-            setUser(rawuser)
         }
-    }, [isMounted])
+    }, [isMounted, user])
 
     if (user && user.id === props.demand.userId && isMounted) {
         return (
@@ -56,12 +54,18 @@ const FulFilmentUserProtection = (props: Props) => {
         if (user && user.id !== props.demand.userId) {
             if (validatingPreviousState) {
                 return (
-                    <div className='w-full pointer-events-none cursor-not-allowed select-none grayscale-25'>{props.children}</div>
+                    <div className='w-full animate-pulse cursor-not-allowed select-none grayscale-25'>
+                        <div className='w-full pointer-events-none '>
+                            {props.children}
+                        </div>
+                    </div>
                 )
             } else {
                 if (hasPlacedOffer) {
                     return (
-                        <div className='w-full'>Your offer has been placed, please check your bidding inbox.</div>
+                        <div className='w-full'>
+                            <div className='mx-auto w-fit p-1 sm:p-2 sm:border sm:border-emerald-100 sm:not-italic bg-emerald-50 rounded italic tracking-tight text-center'>Your offer has been placed, <br />please check your bidding inbox.</div>
+                        </div>
                     )
                 } else {
                     return (
