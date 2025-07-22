15c917ec692230ed21f0f7f7310d28fe3210ea83
Baidar Gul <baidargul@outlook.com>
2025-07-22 13:49:04 +0500

    added active bidding icon

--- components/website/sections/product/list/SectionProductListRow.tsx
+++ components/website/sections/product/list/SectionProductListRow.tsx
@@ -2,7 +2,7 @@ import RatingBar from '@/components/website/ratings/RatingBar'
 import { images } from '@/consts/images'
 import { calculatePricing, formalizeText, formatCurrency } from '@/lib/utils'
 import { Animal } from '@prisma/client'
-import { SquareUserIcon, TruckIcon } from 'lucide-react'
+import { CandlestickChartIcon, SquareUserIcon, TruckIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -21,14 +21,20 @@ const SectionProductListRow = (props: Props) => {
         <Link href={`/entity/${props.animal?.id}`} className='w-full p-2 z-0 bg-white hover:outline-2 hover:outline-zinc-300 group rounded-md shadow-md h-full flex flex-col justify-between'>
             <div className="flex flex-col items-center gap-0">
                 <div className='relative w-full bg-black border-l border-y border-zinc-200/40 rounded-md overflow-hidden'>
-                    {props.animal?.deliveryOptions.length > 0 && <div className='absolute bottom-2  z-[1] right-2 bg-white rounded p-1 flex gap-1 items-center text-zinc-700 border border-zinc-500 drop-shadow-sm'>
-                        {sellerCargo && <div title='SELLER CARGO'>
-                            <TruckIcon size={20} />
+                    {props.animal?.deliveryOptions.length > 0 &&
+                        <div className='absolute bottom-2  z-[1] right-2 bg-white rounded p-1 flex gap-1 items-center text-zinc-700 border border-zinc-500 drop-shadow-sm'>
+                            {sellerCargo && <div title='SELLER CARGO'>
+                                <TruckIcon size={20} />
+                            </div>}
+                            {selfPickup && <div title='SELF PICKUP'>
+                                <SquareUserIcon size={20} />
+                            </div>}
+                            {
+                                props.animal.allowBidding && <div title='ALLOW BIDDING'>
+                                    <CandlestickChartIcon size={20} className='text-amber-500 animate-pulse' />
+                                </div>
+                            }
                         </div>}
-                        {selfPickup && <div title='SELF PICKUP'>
-                            <SquareUserIcon size={20} />
-                        </div>}
-                    </div>}
                     <div className='bg-gradient-to-t from-black/50 to-transparent w-full h-[40%] absolute bottom-0 left-0'></div>
                     <Image src={props.animal.images.length > 0 ? props.animal.images[0].image : images.chickens.images[1]} loading='lazy' layout='fixed' alt='Product List Row' width={1000} height={1000} draggable={false} className='w-full h-[200px] group-hover:scale-105 transition-all duration-300 ease-in-out bg-black select-none object-cover' />
                 </div>
