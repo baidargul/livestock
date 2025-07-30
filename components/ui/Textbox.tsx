1c927482d21a9be76fcac6e07c3af9b31f6f2b40
Baidar Gul <baidargul@outlook.com>
2025-07-30 17:42:42 +0500

    added icon to textbox

--- components/ui/Textbox.tsx
+++ components/ui/Textbox.tsx
@@ -12,10 +12,14 @@ type Props = {
     onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void
     className?: string
     labelClassName?: string
+    icon?: React.ReactNode
+    iconClassName?: string
+    iconHideOnFocus?: boolean
 }
 
 const Textbox = (props: Props) => {
     const txtRef: any = useRef(null)
+    const [isInFocus, setIsInFocus] = useState(false)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (props.onChange) {
@@ -48,7 +52,10 @@ const Textbox = (props: Props) => {
     return (
         <div className='flex flex-col gap-1'>
             {props.label && props.label.length > 0 && <label className={`label ${props.labelClassName}`}>{props.label}</label>}
-            <input id={props.id} ref={txtRef} disabled={props.disabled ?? false} type={props.type ?? "text"} placeholder={props.placeholder} className={`textbox w-full focus-within:tracking-wide ${props.className}`} onChange={handleChange} onKeyDown={handleKeyDown} onFocus={handleOnFocus} value={props.value ?? ""} />
+            <div className='flex gap-1 items-center relative'>
+                {props.icon && <div className={`absolute transition duration-300 left-2 ${isInFocus ? "opacity-0" : "opacity-100"}`}>{props.icon}</div>}
+                <input id={props.id} ref={txtRef} disabled={props.disabled ?? false} type={props.type ?? "text"} placeholder={props.placeholder} onFocusCapture={() => setIsInFocus(true)} onBlurCapture={() => setIsInFocus(false)} className={`textbox w-full focus-within:tracking-wide ${props.className} ${props.icon ? `${isInFocus ? "pl-2" : props.iconClassName}` : null}`} onChange={handleChange} onKeyDown={handleKeyDown} onFocus={handleOnFocus} value={props.value ?? ""} />
+            </div>
         </div>
     )
 }
--- components/website/searchbar/ProductSearchBar.tsx
+++ components/website/searchbar/ProductSearchBar.tsx
@@ -1,5 +1,5 @@
 import Textbox from '@/components/ui/Textbox'
-import { SlidersHorizontalIcon } from 'lucide-react'
+import { Search, SlidersHorizontalIcon } from 'lucide-react'
 import React from 'react'
 
 type Props = {}
@@ -7,12 +7,12 @@ type Props = {}
 const ProductSearchBar = (props: Props) => {
     return (
         <div className='flex w-full gap-2 items-center px-4 mb-4'>
-            <div className='w-full'>
-                <Textbox placeholder='Search for a new freind' />
+            <div className='w-full mt-2'>
+                <Textbox placeholder='Search for a new freind' icon={<Search size={20} className='text-zinc-400' />} iconClassName='pl-10' />
             </div>
-            <div className='bg-emerald-100 text-emerald-600 border border-emerald-200 cursor-pointer p-2 rounded'>
+            {/* <div className='bg-emerald-100 text-emerald-600 border border-emerald-200 cursor-pointer p-2 rounded'>
                 <SlidersHorizontalIcon />
-            </div>
+            </div> */}
         </div>
     )
 }
