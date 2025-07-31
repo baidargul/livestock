059cafff11972a595366a0f23f5c7902ecff5a49
Baidar Gul <baidargul@outlook.com>
2025-07-31 13:08:06 +0500

    working on search bar again fixed overlay effect

--- components/website/searchbar/ProductSearchBar.tsx
+++ components/website/searchbar/ProductSearchBar.tsx
@@ -14,12 +14,15 @@ const ProductSearchBar = (props: Props) => {
     }
 
     return (
-        <div className='flex w-full gap-2 items-center px-4 mb-4'>
-            <div className='w-full mt-2 relative'>
-                <Textbox onChange={handleTextChange} value={searchCriteria} placeholder='Search for anything' icon={<Search size={20} className='text-zinc-400' />} iconClassName='pl-10' />
-                {searchCriteria && searchCriteria.length > 0 && < SearchResults criteria={searchCriteria} setCriteria={setSearchCriteria} />}
+        <>
+            <div className='flex w-full gap-2 items-center px-4 mb-4 z-[2]'>
+                <div className='w-full mt-2 relative'>
+                    <Textbox onChange={handleTextChange} value={searchCriteria} placeholder='Search for anything' icon={<Search size={20} className='text-zinc-400' />} iconClassName='pl-10' />
+                    {searchCriteria && searchCriteria.length > 0 && < SearchResults criteria={searchCriteria} setCriteria={setSearchCriteria} />}
+                </div>
             </div>
-        </div>
+            {searchCriteria.length > 0 && <div onClick={() => setSearchCriteria('')} className='inset-0 fixed w-full h-full z-[1]'></div>}
+        </>
     )
 }
 
