0cac7840bbb148347805deb204e1902a8f54bf96
Baidar Gul <baidargul@outlook.com>
2025-07-31 12:00:23 +0500

    created Contacts hook

---
+++ hooks/useContacts.ts
@@ -0,0 +1,32 @@
+import { actions } from "@/actions/serverActions/actions";
+import { create } from "zustand";
+
+export const useContacts = create<any>()((set) => ({
+  contacts: [],
+  isFetching: false,
+  addToContact: (contact: any) => {
+    set({ isFetching: true });
+    const prev = useContacts.getState();
+    prev.contacts.push(contact);
+    set({ isFetching: false, contacts: prev.contacts });
+  },
+  removeContact: (userId: string) => {
+    const prev = useContacts.getState();
+    prev.contacts = prev.contacts.filter(
+      (contact: any) => contact.userId !== userId
+    );
+    set(prev);
+  },
+  findContact: (userId: string) => {
+    const prev = useContacts.getState();
+    return prev.contacts.find((contact: any) => contact.userId === userId);
+  },
+  fetchContacts: async (userId: string) => {
+    const response = await actions.client.user.contacts.listAll(userId);
+    if (response.status === 200) {
+      set({ contacts: response.data });
+    } else {
+      set({ contacts: [] });
+    }
+  },
+}));
