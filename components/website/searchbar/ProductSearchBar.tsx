900bf869c3a9dc6bd36d46f36124d87cc76f0131
Baidar Gul <baidargul@outlook.com>
2025-07-31 19:54:32 +0500

    searchbox ui

--- components/website/searchbar/ProductSearchBar.tsx
+++ components/website/searchbar/ProductSearchBar.tsx
@@ -15,13 +15,13 @@ const ProductSearchBar = (props: Props) => {
 
     return (
         <>
-            <div className='flex w-full gap-2 items-center px-4 mb-4 z-[2]'>
-                <div className='w-full mt-2 relative'>
-                    <Textbox onChange={handleTextChange} value={searchCriteria} placeholder='Search for anything' icon={<Search size={20} className='text-zinc-400' />} iconClassName='pl-10' />
+            <div className='flex w-full gap-2 items-center px-4 mb-4 sticky z-[4]'>
+                <div className='w-full mt-2 relative bg-white'>
+                    <Textbox onChange={handleTextChange} value={searchCriteria} placeholder='Search for anything' icon={<Search size={20} className='text-zinc-400 bg-white' />} iconClassName='pl-10' />
                     {searchCriteria && searchCriteria.length > 0 && < SearchResults criteria={searchCriteria} setCriteria={setSearchCriteria} />}
                 </div>
             </div>
-            {searchCriteria.length > 0 && <div onClick={() => setSearchCriteria('')} className='inset-0 fixed w-full h-full z-[1]'></div>}
+            {searchCriteria.length > 0 && <div onClick={() => setSearchCriteria('')} className='inset-0 fixed w-full h-full z-[1] bg-emerald-600/40 backdrop-blur-[1px]'></div>}
         </>
     )
 }
