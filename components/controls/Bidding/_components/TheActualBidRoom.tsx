eb54211d9f6b4495af02940acfd30aec6b91633c
Baidar Gul <baidargul@outlook.com>
2025-08-03 20:02:12 +0500

    added additional information of about current running rates

--- components/controls/Bidding/_components/TheActualBidRoom.tsx
+++ components/controls/Bidding/_components/TheActualBidRoom.tsx
@@ -2,6 +2,7 @@ import DeliveryIcon from '@/components/Animals/DeliveryIcon'
 import { calculatePricing, formatCurrency } from '@/lib/utils'
 import { ChevronLeftIcon } from 'lucide-react'
 import React from 'react'
+import UserAndRoomContextQuickView from './UserAndRoomContextQuickView'
 
 type Props = {
     handleLeaveRoom: any
@@ -19,16 +20,18 @@ const TheActualBidRoom = (props: Props) => {
                     <div><ChevronLeftIcon onClick={() => props.handleLeaveRoom(!props.isAuthor)} className='w-6 h-6 cursor-pointer' /></div>
                     <div className='flex items-center gap-1'>{props.socketState.isOtherUserConnected ? <div className='w-2 h-2 bg-emerald-500 rounded-full'></div> : <div className='w-2 h-2 bg-amber-500 rounded-full'></div>} <div className='line-clamp-1'>{props.isAuthor ? props.activeBidRoom.user.name : props.activeBidRoom.author.name}<div className='text-xs font-normal italic -mt-1'>{props.isAuthor ? "Buyer" : "Seller"}</div></div></div>
                 </div>
-                <div className='text-sm tracking-wide text-nowrap'>
-                    {props.activeBidRoom.bids.length > 0 && <div>
-                        <div className='p-1 px-2 text-center bg-amber-100 rounded-md'>
-                            {formatCurrency(props.activeBidRoom.bids.length > 0 && props.activeBidRoom.bids[props.activeBidRoom.bids.length - 1]?.price)}
-                        </div>
-                        <div className='p-1 px-2 text-center border-t pt-1 border-zinc-300'>
-                            {formatCurrency(calculatePricing({ ...props.animal, maleQuantityAvailable: props.activeBidRoom.maleQuantityAvailable, femaleQuantityAvailable: props.activeBidRoom.femaleQuantityAvailable }).price)}
-                        </div>
-                    </div>}
-                </div>
+                <UserAndRoomContextQuickView isAuthor={props.isAuthor} animal={props.animal} otherUser={props.isAuthor ? props.activeBidRoom.user : props.activeBidRoom.author} room={props.activeBidRoom}>
+                    <div className='text-sm tracking-wide text-nowrap'>
+                        {props.activeBidRoom.bids.length > 0 && <div>
+                            <div className='p-1 px-2 text-center bg-amber-100 rounded-md'>
+                                {formatCurrency(props.activeBidRoom.bids.length > 0 && props.activeBidRoom.bids[props.activeBidRoom.bids.length - 1]?.price)}
+                            </div>
+                            <div className='p-1 px-2 text-center border-t pt-1 border-zinc-300'>
+                                {formatCurrency(calculatePricing({ ...props.animal, maleQuantityAvailable: props.activeBidRoom.maleQuantityAvailable, femaleQuantityAvailable: props.activeBidRoom.femaleQuantityAvailable }).price)}
+                            </div>
+                        </div>}
+                    </div>
+                </UserAndRoomContextQuickView>
             </div>
             <div className="text-xs tracking-wide -mt-3 px-6 flex gap-1 items-center">
                 <div className='border-r-2 border-zinc-300 pr-4 flex gap-1 items-center'>
---
+++ components/controls/Bidding/_components/UserAndRoomContextQuickView.tsx
@@ -0,0 +1,86 @@
+
+'use client'
+import DeliveryIcon from '@/components/Animals/DeliveryIcon'
+import { calculatePricing, formalizeText, formatCurrency } from '@/lib/utils'
+import { XIcon } from 'lucide-react'
+import React, { useState } from 'react'
+
+type Props = {
+    animal: any
+    otherUser: any
+    room: any
+    children: React.ReactNode
+    isAuthor: boolean
+}
+
+const UserAndRoomContextQuickView = (props: Props) => {
+    const [isToggled, setIsToggled] = useState(false)
+    const totalAnimals = Number(props.room.maleQuantityAvailable) + Number(props.room.femaleQuantityAvailable)
+    const runningValue = props.room.bids[props.room.bids.length - 1]?.price
+    const perUnitValue = Number(Number(runningValue / totalAnimals).toFixed(0))
+    const perUnitCost = Number(Number(calculatePricing({ ...props.animal, ...props.room }).price / totalAnimals).toFixed(0))
+    const difference = Number(perUnitValue) - Number(perUnitCost)
+
+    const handleToggle = (val: boolean) => {
+        if (props.isAuthor && props.isAuthor === true) {
+            setIsToggled(val)
+        }
+    }
+    return (
+        <div className='relative'>
+            {isToggled && <div className='text-base font-normal absolute top-0 right-0 w-full min-w-[200px] min-h-[200px] bg-white shadow-sm rounded p-2 z-[1]'>
+                <div className='flex gap-2 items-start justify-between'>
+                    <div className='text-lg leading-4'>
+                        {props.otherUser?.name}
+                    </div>
+                    <XIcon onClick={() => handleToggle(false)} className='cursor-pointer' />
+                </div>
+                <div className='text-xs tracking-tight'>{formalizeText(props.room?.user?.city)}, {formalizeText(props.room?.user?.province)}</div>
+                <div className='mt-4'>
+                    <div className='font-semibold'>Demands</div>
+                    <div className='text-xs'>
+                        <div>
+                            {props.room.maleQuantityAvailable} Males, {props.room.femaleQuantityAvailable} Females = {totalAnimals} {props.room.animal.type}
+                        </div>
+
+                        <div className='text-xs font-semibold mt-1'>Delivery:</div>
+                        <div className='pr-4 flex gap-1 items-center'>
+                            {
+                                props.room.deliveryOptions.map((option: any, index: number) => {
+
+
+                                    return (
+                                        <div key={`${option}-${index}`} className='flex flex-col items-center text-center'>
+                                            <DeliveryIcon icon={option} />
+                                            <div className='text-xs'>{option === "SELLER_DELIVERY" ? "By Cargo" : "Self Pickup"}</div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+
+                    </div>
+                </div>
+                <div className='mt-4 leading-5 scale-75 origin-top-left'>
+                    <div className=''>
+                        <div>
+                            Posted:
+                        </div>
+                        <div className='text-base'>
+                            {formatCurrency(perUnitCost)} per animal.
+                        </div>
+                    </div>
+                    <div>
+                        Offered <span className={`tracking-tight font-semibold ${perUnitValue > perUnitCost ? "text-green-700" : perUnitValue === perUnitCost ? "text-amber-700" : "text-red-700"}`}>{formatCurrency(perUnitValue)}</span> per animal.
+                    </div>
+                </div>
+                <div>
+                    Difference <span className={`tracking-tight font-semibold ${perUnitValue > perUnitCost ? "text-green-700" : perUnitValue === perUnitCost ? "text-amber-700" : "text-red-700"}`}>{formatCurrency(difference)}</span> per animal.
+                </div>
+            </div>}
+            <div className='cursor-pointer' onClick={() => handleToggle(true)}>{props.children}</div>
+        </div>
+    )
+}
+
+export default UserAndRoomContextQuickView
