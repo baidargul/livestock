22699aa43d8d364e43ff5ff30533013883a8d691
Baidar Gul <baidargul@outlook.com>
2025-08-04 12:09:44 +0500

    ...

--- components/website/sections/product/list/SectionProductListRow.tsx
+++ components/website/sections/product/list/SectionProductListRow.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { images } from '@/consts/images'
 import { calculatePricing, formalizeText, formatCurrency } from '@/lib/utils'
 import { Animal } from '@prisma/client'
@@ -6,19 +7,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import CTOButton from './_components/CTOButton'
+import { useUser } from '@/socket-client/SocketWrapper'
 
 type Props = {
     animal: Animal | any
 }
 
 const SectionProductListRow = (props: Props) => {
+    const user = useUser()
 
     const totalQuantity = Number(props.animal?.maleQuantityAvailable || 0) + Number(props.animal?.femaleQuantityAvailable || 0)
 
     const sellerCargo = props.animal?.deliveryOptions.includes("SELLER_DELIVERY")
     const selfPickup = props.animal?.deliveryOptions.includes("SELF_PICKUP")
     return (
-        <Link href={`/entity/${props.animal?.id}`} prefetch={true} className='break-inside-avoid-column w-full shadow-sm p-2 z-0 bg-white hover:outline-2 hover:outline-zinc-300 group h-full flex flex-col justify-between'>
+        <Link href={`/entity/${props.animal?.id}`} prefetch={true} className={`break-inside-avoid-column w-full shadow-sm p-2 z-0 ${user?.id === props.animal.userId ? "bg-white" : "bg-white"}  hover:outline-2 hover:outline-zinc-300 group h-full flex flex-col justify-between`}>
             <div className="flex flex-col items-center gap-0">
                 <div className='relative w-full bg-black border-l border-y border-zinc-200/40 overflow-hidden'>
                     {(props.animal?.deliveryOptions.length > 0 || props.animal?.allowBidding) &&
@@ -45,22 +48,12 @@ const SectionProductListRow = (props: Props) => {
                             <span className=''>{totalQuantity}</span> <span className=''>{`${props.animal?.breed} ${String(props.animal?.type).slice(0, props.animal?.type.length - 1)}`}</span>
                         </span>
                     </div>
-                    {/* <div className=''>
-                        {Number(props.animal?.maleQuantityAvailable ?? 0) > 0 && <span className='text-sm'>{props.animal?.maleQuantityAvailable} Male</span>}
-                        {Number(props.animal?.femaleQuantityAvailable ?? 0) > 0 && <span className='text-sm'> {props.animal?.femaleQuantityAvailable} Female</span>}
-                    </div> */}
                     {props.animal?.user?.name && props.animal?.user?.name.length > 0 && <div className='flex flex-col gap-1'>
-                        {/* <div className='font-medium text-zinc-800 tracking-tight text-base text-balance leading-4 -mb-1'>
-                            {props.animal?.user?.name}
-                        </div> */}
                         {props.animal?.city && props.animal?.province && props.animal?.city.length > 0 && props.animal?.province.length > 0 && <div className='text-xs text-zinc-700 tracking-tight mb-2 flex gap-1 items-center'> <MapPinIcon size={14} /> {formalizeText(props.animal?.city)}, {formalizeText(props.animal?.province)}</div>}
                     </div>}
                 </div>
             </div>
             <CTOButton animal={props.animal} />
-            {/* <div className='text-2xl sm:text-xl md:text-lg text-nowrap text-left text-emerald-600 tracking-wide font-bold'>
-                {formatCurrency(calculatePricing(props.animal).price)}
-            </div> */}
         </Link >
     )
 }
