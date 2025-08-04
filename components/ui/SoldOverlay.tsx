361b0b462057dc36d88f625e8f8f073cce5fa98c
Baidar Gul <baidargul@outlook.com>
2025-08-04 17:13:59 +0500

    ...

--- components/ui/SoldOverlay.tsx
+++ components/ui/SoldOverlay.tsx
@@ -37,7 +37,7 @@ const SoldOverlay = (props: Props) => {
     return (
         <div className='w-full h-full relative'>
             {props.children}
-            {animal?.sold && <div className='absolute bottom-0 left-0 z-50 pointer-events-none w-full h-full bg-gradient-to-t from-white to-transparent flex justify-center items-center'>
+            {animal?.sold && <div className='absolute bottom-0 left-0 z-[1] pointer-events-none w-full h-full bg-gradient-to-t from-white to-transparent flex justify-center items-center'>
                 <Image src={images.site.ui.sold} alt='sold' width={100} height={100} layout='fixed' className='w-full mt-auto' />
             </div>}
         </div>
