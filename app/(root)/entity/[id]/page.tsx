9b2c7d02ae368e69f16002460a31453708c418d1
Baidar Gul <baidargul@outlook.com>
2025-08-19 20:51:04 +0500

    added location to entity page

--- app/(root)/entity/[id]/page.tsx
+++ app/(root)/entity/[id]/page.tsx
@@ -68,7 +68,7 @@ const page = async (props: Props) => {
                         </div>
                     </div>
                     <section>
-                        <p className='text-lg text-gray-600'>{animal.description}</p>
+                        <p className='text-lg text-gray-600 tracking-tight'>'{animal.description}'</p>
                         <ElapsedTimeControl date={animal.createdAt} />
                         <SoldOverlay animal={animal}>
                             <CalculatedDescription animal={animal} />
@@ -113,15 +113,18 @@ const page = async (props: Props) => {
                         <div className='px-4 py-2'>
                             <p className='text-sm text-gray-600'>{info ?? ''}</p>
                         </div>
-                        <ElapsedTimeControl date={animal.createdAt} />
                     </div>
                 </div>
                 <div className='mt-40 p-4'>
                     {animal.allowBidding && !animal.sold && <Marquee className='mb-4 border-y-2 pointer-events-none border-amber-500 bg-amber-50 w-full'>
                         <div className='font-semibold p-1 tracking-widest italic text-amber-700 scale-75 -mb-2 origin-top-left'>Bidding is active for this animal.</div>
                     </Marquee>}
-                    <h2 className='text-2xl font-bold text-gray-800'>{animal.title}</h2>
-                    <p className='text-lg text-gray-600'>{animal.description}</p>
+                    <h2 className='text-2xl mb-1 font-bold text-gray-800'>{animal.title}</h2>
+                    <p className='text-sm p-1 text-zinc-800 tracking-tight bg-slate-50 h-auto min-h-[100px]'>'{animal.description}'</p>
+                    <ElapsedTimeControl date={animal.createdAt} />
+                    <div className='border-t-2 border-zinc-300 my-2 p-1 w-full flex justify-end items-center'>
+                        <div className='text-xs font-bold tracking-wide'>{formalizeText(animal.city)}, {formalizeText(animal.province)}</div>
+                    </div>
                     <SoldOverlay animal={animal}>
                         <CalculatedDescription animal={animal} />
                     </SoldOverlay>
