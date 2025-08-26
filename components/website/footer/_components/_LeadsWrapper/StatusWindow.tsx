94ce156ee4d3feb28960cdfb8d0df6c10394bf43
Baidar Gul <baidargul@outlook.com>
2025-08-26 21:27:21 +0500

    ...

--- components/website/footer/_components/_LeadsWrapper/StatusWindow.tsx
+++ components/website/footer/_components/_LeadsWrapper/StatusWindow.tsx
@@ -43,7 +43,7 @@ const StatusWindow = (props: Props) => {
     return (
         <>
             <div className={`${isOpen ? "scale-[1] opacity-100 pointer-events-auto" : "scale-[.2] opacity-0 pointer-events-none"} transition ease-in-out duration-300 inset-0 fixed top-0 left-0 z-[3] flex justify-center items-center`}>
-                <section className='p-2 bg-white shadow-sm rounded-md'>
+                <section className='p-4 bg-white shadow-sm rounded-md'>
                     <div className='font-bold flex gap-1 items-center'>{formalizeText(props.lead.user.name)}</div>
                     {String(props.lead.city).length > 0 && String(props.lead.province).length > 0 && <div className='text-zinc-600 text-xs'>Delivery location: <span className='font-bold'>{formalizeText(props.lead.city)}, {formalizeText(props.lead.province)}</span></div>}
                     {String(props.lead.city).length === 0 && String(props.lead.province).length === 0 && <div className='text-zinc-600 text-xs'>{formalizeText(props.lead.user.city)}, {formalizeText(props.lead.user.province)}</div>}
@@ -68,21 +68,21 @@ const StatusWindow = (props: Props) => {
                         <ElapsedTimeControl date={props.lead.createdAt} />
                     </div>
                     <div className={`my-2 ${props.lead.status === "dispatched" ? "pointer-events-none opacity-50 grayscale-100" : "pointer-events-auto"}`}>
-                        <div className='font-bold mb-2'>Order Status</div>
+                        <div className='font-bold mb-2'>Current order status:</div>
                         <div className='grid grid-cols-3 gap-2'>
                             <Button disabled={newStatus.length > 0} onClick={() => handleChangeStatus('pending')} variant={props.lead.status === 'pending' ? "btn-primary" : "btn-secondary"}>Pending</Button>
                             <Button disabled={newStatus.length > 0} onClick={() => handleChangeStatus('dispatched')} variant={props.lead.status === 'dispatched' ? "btn-primary" : "btn-secondary"}>Dispatched</Button>
                             <Button disabled={newStatus.length > 0} onClick={() => handleChangeStatus('cancelled')} variant={props.lead.status === 'cancelled' ? "btn-primary" : "btn-secondary"}>Cancelled</Button>
                         </div>
                     </div>
-                    <Button onClick={() => handleOpen(false)} className='w-full'>Close</Button>
+                    <Button onClick={() => handleOpen(false)} className='w-full mt-4'>Close</Button>
                 </section>
             </div>
             <div onClick={() => handleOpen(true)}>
                 {props.children}
             </div>
-            {isOpen && <div onClick={() => handleOpen(false)} className='inset-0 fixed top-0 left-0 z-[2]'></div>}
-            {isOpen && <div className='fixed top-0 left-0 bg-black/40 blur-[1px] inset-0 z-[1]'></div>}
+            {isOpen && <div onClick={() => handleOpen(false)} className='inset-0 fixed backdrop-blur-[1px] top-0 left-0 z-[2]'></div>}
+            {isOpen && <div onClick={() => handleOpen(false)} className='fixed top-0 left-0 bg-black/40 inset-0 z-[1]'></div>}
         </>
     )
 }
