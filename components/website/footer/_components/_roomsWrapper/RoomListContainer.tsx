99fec41452d387a922d0590a316edc3d2cce2d06
Baidar Gul <baidargul@outlook.com>
2025-08-27 18:43:39 +0500

    ...

--- components/website/footer/_components/_roomsWrapper/RoomListContainer.tsx
+++ components/website/footer/_components/_roomsWrapper/RoomListContainer.tsx
@@ -12,7 +12,7 @@ const RoomListContainer = (props: Props) => {
     return (
         <>
             <div onClick={() => props.handleToggleMenu(false)} className={`bg-black/50 backdrop-blur-[1px] inset-0 fixed ${props.isToggled ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}></div>
-            <div className={`w-[90%] sm:w-[40%] select-none transition-all duration-200 z-[10] ease-out h-full fixed right-0 ${props.isToggled ? "translate-x-0" : " translate-x-full"} top-0 bg-white`}>
+            <div className={`w-[90%] sm:w-[40%] select-none transition-all duration-200 z-50 ease-out h-full fixed right-0 ${props.isToggled ? "translate-x-0" : " translate-x-full"} top-0 bg-white`}>
                 <Rooms rooms={props.rooms} user={props.user} />
             </div>
         </>
