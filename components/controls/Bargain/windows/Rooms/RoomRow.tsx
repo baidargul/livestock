9f24f6830d0efd1fa33fe092a38725e9b6b9a231
Baidar Gul <baidargul@outlook.com>
2025-09-22 18:19:49 +0500

    selling ui complete

--- components/controls/Bargain/windows/Rooms/RoomRow.tsx
+++ components/controls/Bargain/windows/Rooms/RoomRow.tsx
@@ -1,8 +1,11 @@
-import ElapsedTimeControl from '@/components/controls/ElapsedTimeControl'
+'use client'
+import { LuHandshake } from "react-icons/lu";
 import { formalizeText, formatCurrency } from '@/lib/utils'
 import { useUser } from '@/socket-client/SocketWrapper'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import LastMessage from './LastMessage'
+import { Bids } from '@prisma/client'
+import { bidRoom } from '@/actions/serverActions/server/partials/bidroom'
 
 type Props = {
     room: any
@@ -11,16 +14,35 @@ type Props = {
 }
 
 const RoomRow = (props: Props) => {
+    const [closedBid, setClosedBid] = useState<Bids | null>(null)
     const user = useUser()
     const room = props.room
     const isBuyingRoom = room.userId === user.id
     const isSelfPickUp = !room.deliveryOptions.includes("SELLER_DELIVERY")
 
+    const isDealClosed = room.closedAt && String(room.closedAt).length > 0
+
+    useEffect(() => {
+        if (isDealClosed && user) {
+            let selectedBid = null
+            room.bids.forEach((bid: Bids) => {
+                if (bid.selected && bid.selected === true) {
+                    selectedBid = bid
+                }
+            })
+            if (selectedBid) {
+                setClosedBid(selectedBid)
+            } else {
+                setClosedBid(null)
+            }
+        }
+    }, [isDealClosed, user])
+
     if (isBuyingRoom) {
         return (
             <div onClick={() => props.handleSelectCurrentRoom(room)} className='bg-white cursor-pointer p-2 rounded-md flex justify-between items-center'>
                 <div>
-                    <div className='font-semibold line-clamp-1 text-black text-xl'>
+                    <div className={`font-semibold line-clamp-1 text-black text-xl`}>
                         {room.animal.user.name}
                     </div>
                     <div className='text-zinc-500 text-sm'>
@@ -34,14 +56,21 @@ const RoomRow = (props: Props) => {
         return (
             <div onClick={() => props.handleSelectCurrentRoom(room)} className='bg-white cursor-pointer p-2 rounded-md flex justify-between items-center'>
                 <div>
-                    <div className='font-semibold line-clamp-1 text-black text-xl'>
+                    <div className={`font-semibold line-clamp-1 flex items-center gap-1 text-black text-xl ${closedBid && closedBid.userId !== user.id ? 'line-through text-zinc-600' : ''}`}>
+                        {closedBid && <LuHandshake className={`${closedBid && closedBid.userId !== user.id ? "hidden" : "text-emerald-700 fill-emerald-50 "}`} />}
                         {room.user.name}
                     </div>
-                    <div className='text-zinc-500 text-sm'>
-                        {!isSelfPickUp ? `${formalizeText(room.city)}, ${formalizeText(room.province)}` : "I'll self pickup"}
-                    </div>
+                    {closedBid && closedBid.userId !== user.id ? <div className='text-zinc-500 text-sm'>
+                        Rejected
+                    </div> :
+                        <div className='text-zinc-500 text-sm'>
+                            {!isSelfPickUp ? `${formalizeText(room.city)}, ${formalizeText(room.province)}` : "I'll self pickup"}
+                        </div>
+                    }
                 </div>
-                <LastMessage lastBid={room.bids[room.bids.length - 1] ?? null} />
+                <div className={`${closedBid && closedBid.userId !== user.id ? 'line-through text-zinc-600' : ''}`}>
+                    <LastMessage lastBid={room.bids[room.bids.length - 1] ?? null} />
+                </div>
             </div>
         )
     }
